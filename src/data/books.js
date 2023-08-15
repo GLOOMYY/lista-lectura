a9490bc4c1a2c25a34ef2e6f44c7bf9debe29fd5
GLOOMYY <sebas.mesa.montoya@gmail.com>
2023-08-15 12:29:33 -0500

    Feat: Pages

---
+++ src/data/books.js
@@ -0,0 +1,44 @@
+export const books = [
+  {
+    id: 'clean-code',
+    title: 'Clean Code: Manual de desarrollo ágil de software',
+    img: 'https://m.media-amazon.com/images/I/41SH-SvWPxL.jpg',
+    author: 'Robert C. Martin',
+    opinion: 'Esta es una prueba'
+  },
+  {
+    id: 'the-clean-coder',
+    title: 'The Clean Coder: A Code of Conduct for Professional Programmers',
+    img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1347470803i/10284614.jpg',
+    author: 'Robert C. Martin',
+    opinion: ''
+  },
+  {
+    id: 'desing-patterns',
+    title: 'Design Patterns: Elements of Reusable Object-Oriented Software',
+    img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1348027904i/85009.jpg',
+    author: ' Erich Gamma, Richard Helm, Ralph Johnson, John Vlissides',
+    opinion: ''
+  },
+  {
+    id: 'the-pragmatic-programmer',
+    title: 'The Pragmatic Programmer: From Journeyman to Master',
+    img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1401432508i/4099.jpg',
+    author: 'Andy Hunt, Dave Thomas',
+    opinion: ''
+  },
+  {
+    id: 'refactoring',
+    title: 'Refactoring: Improving the Design of Existing Code',
+    img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1386925632i/44936.jpg',
+    author: 'Martin Fowler, Kent Beck',
+    opinion: ''
+  },
+  {
+    id: 'clean-architecture',
+    title: 'Clean Architecture',
+    img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1471680093i/18043011.jpg',
+    author: 'Robert C. Martin',
+    opinion: ''
+  }
+]
